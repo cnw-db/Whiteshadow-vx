a4dbf3806bb67368a62b63d4e1895d9562a7d7b6
cnw-db <newbot1952@gmail.com>
2025-10-31 00:20:10 +0530

    Update ai3.js

--- plugins/ai3.js
+++ plugins/ai3.js
@@ -9,19 +9,35 @@ cmd({
   use: ".ai3 <question>",
   react: "🤖",
   filename: __filename
-}, async (m, { text, reply }) => {
-  if (!text) return reply("🧠 *Please enter a message to ask AI.*\nExample: .ai3 What is cyber security?");
+}, async (m, { sock, text, reply }) => {
+  if (!text) {
+    return await sock.sendMessage(m.chat, {
+      text: "🧠 *Please enter a message to ask AI.*\nExample: .ai3 What is cyber security?",
+      ai: true
+    });
+  }
 
   try {
     let res = await axios.get(`https://whiteshadow-thz2.onrender.com/ai/gpt-5-mini?query=${encodeURIComponent(text)}`);
+    
     if (res.data && res.data.status && res.data.answer) {
-      return reply(res.data.answer);
+      return await sock.sendMessage(m.chat, {
+        text: `🤖 *WhiteShadow AI:*\n\n${res.data.answer}`,
+        ai: true
+      });
     } else {
       console.error(res.data);
-      return reply("⚠️ AI response not received properly.");
+      return await sock.sendMessage(m.chat, {
+        text: "⚠️ *AI response not received properly.*",
+        ai: true
+      });
     }
+
   } catch (err) {
     console.error(err);
-    return reply("❌ *Error connecting to WHITESHADOW AI server.*");
+    return await sock.sendMessage(m.chat, {
+      text: "❌ *Error connecting to WHITESHADOW AI server.*",
+      ai: true
+    });
   }
 });

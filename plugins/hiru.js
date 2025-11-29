6933307a8d074f2792f61b01b0c0526c5b96443d
Chamod Nimsara <newbot1952@gmail.com>
2025-11-29 22:02:23 +0530

    Update hiru.js

--- plugins/hiru.js
+++ plugins/hiru.js
@@ -34,3 +34,67 @@ cmd({
     }
   }
 });
+
+
+
+
+cmd({
+    pattern: "derana",
+    alias: ["news2", "derananews"],
+    category: "news",
+    react: "📰",
+    desc: "Get latest AdaDerana Sinhala news"
+}, 
+
+async (msg, args, client) => {
+    try {
+
+        // API URL
+        const apiURL = "https://derana.vercel.app/api/derana";
+
+        const { data } = await axios.get(apiURL);
+
+        if (!data.status) {
+            return client.sendMessage(
+                msg.from,
+                { text: "❌ Failed to fetch news!" },
+                { quoted: msg }
+            );
+        }
+
+        const n = data.result;
+
+        // WhatsApp Caption
+        const caption =
+`📰 *AdaDerana පුවත් (Latest)*
+  
+*📌 ශීර්ෂය:* ${n.title}
+
+*📅 දිනය:* ${n.date}
+
+*📝 විස්තර:*  
+${n.desc ? n.desc.substring(0, 800) : n.description}
+
+🔗 *සම්පූර්ණ පුවත:*  
+${n.url};
+
+        // Send news with image
+        await client.sendMessage(
+            msg.from,
+            {
+                image: { url: n.image },
+                caption
+            },
+            { quoted: msg }
+        );
+
+    } catch (e) {
+        console.log(e);
+        client.sendMessage(
+            msg.from,
+            { text: "⚠️ Error fetching Derana news!" },
+            { quoted: msg }
+        );
+    }
+});
+
